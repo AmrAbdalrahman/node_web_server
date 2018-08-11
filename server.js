ed77c32cd29b731bde2bac7c416d0fe25eebcfd9
AmrAhmed94 <amr.abdalrahman94@gmail.com>
2018-08-11 17:13:48 +0200

    setup start script and heroke port

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 
@@ -15,8 +18,14 @@ app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
 
-    console.log(log);
-    fs.appendFile('server.log', log + '\n');
+    /*console.log(log);*/
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+
+
+    /*fs.appendFile('server.log', log + '\n', 'utf8', callback);*/
     next();
 });
 
@@ -65,6 +74,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
